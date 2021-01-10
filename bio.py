b0e76ddda32b77af4452d33ff7e61d8f01c79a7c
Iknoor.Singh <66911222+iknoors@users.noreply.github.com>
2021-01-10 09:17:28 -0500

    Add files via upload

---
+++ bio.py
@@ -0,0 +1,12 @@
+firstname = input("PLease enter your name:  ")
+lastname = input("PLease enter your last name:  ")
+age = input("PLease enter your age:")
+born = input("PLease enter your birth info example 7/14/11:")
+print ("Here is the info you typed")
+print(firstname)
+
+print(lastname)
+
+print(age)
+
+print(born)
